fe27e8c64cb2fbab4c86d37941b43adbe01000da
KNT1962 <51218213+kttmu@users.noreply.github.com>
2020-05-17 23:00:36 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 #1. clone the repository dicternal for linux
 #2. execute the run.sh to set up 
 #3. and you can use it any time
-echo "export ${HOME}/dict/" >> ${HOME}/.bashrc
+echo "export PATH=$PATH:$HOME/dict/" >> $HOME/.bashrc
+source ~/.bashrc
