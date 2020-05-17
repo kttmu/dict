4bd1c1b5490697c71ea93ee34a69f3ae2e959847
KNT1962 <51218213+kttmu@users.noreply.github.com>
2020-05-17 22:56:17 +0900

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,5 @@
+#run to start dicternal
+#1. clone the repository dicternal for linux
+#2. execute the run.sh to set up 
+#3. and you can use it any time
+echo "export ${HOME}/dict/" >> ${HOME}/.bashrc
